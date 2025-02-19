0ce29f24fa7a8cfac085ef62518c3b45bb53a33c
Luis Amorim <luis@amorim.bio.br>
2025-02-19 10:13:45 -0300

    Initial Commit

---
+++ macMonitor/macMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  macMonitorApp.swift
+//  macMonitor
+//
+//  Created by Luis Amorim on 19/02/25.
+//
+
+import SwiftUI
+
+@main
+struct macMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
